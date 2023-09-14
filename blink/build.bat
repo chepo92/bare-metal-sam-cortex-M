b54b837fe13bd74deca80b6527dbab03e365988b
Axel Sepulveda <ansepulveda@uc.cl>
2023-09-13 22:53:00 -0300

    update bossac script

--- blink/build.bat
+++ blink/build.bat
@@ -2,7 +2,7 @@
 
 set TC=%USERPROFILE%\AppData\Local\Arduino15\packages\arduino\tools\arm-none-eabi-gcc\4.8.3-2014q1\bin
 set CC=%TC%\arm-none-eabi-gcc
-set PROG=%USERPROFILE%\AppData\Local\Arduino15\packages\arduino\tools\bossac\bossac-1.7.0
+set PROG=%USERPROFILE%\.platformio\packages\tool-bossac
 
 set CFLAGS=-mcpu=cortex-m3 -mthumb --specs=nosys.specs -nostdlib -g -Os
 
@@ -17,9 +17,20 @@ echo Creating bin file...
 %TC%\arm-none-eabi-objcopy  -O binary  blink.elf blink.bin
 
 
-if not /%1/ == /PROG/ goto skip1
 
 echo Programming code...
-%PROG%\bossac --port=COM68 -U true -i -u -e -w -b "blink.bin" -v -R
+
+%PROG%\bossac --info --debug --port "COM7" --write --verify --reset --erase -U false --boot "blink.bin"
+
+
+if not /%1/ == /PROG/ goto skip1
+%PROG%\bossac --port=COM5 -U true -i -u -e -w -b "blink.bin" -v -R
+
+\AppData\Local\Arduino15\packages\arduino\tools\bossac\1.6.1-arduino
+echo Touch port...
+D:\Projects\Python\env3.11\Scripts\python.exe touchPort.py
+
+echo Wait...
+powershell -nop -c "& {sleep -m 400}"
 
 :skip1
